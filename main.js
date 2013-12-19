c2bf59e6001dc69e23fa9b421e706ffa8e5673a0
Maksim Lin <maks@manichord.com>
2013-12-19 12:19:30 +1100

    add stop/go buttons

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+var STOP = false;
+
 window.onload = function() {
   console.log("init");
   
   var count = 1;
   var DELAY = 25;//ms
-  var MAX = 100;
+  var MAX = 100;  
   
   setInterval(function() {
+      if (STOP) {
+        return;
+      }
       var imgEl = document.getElementById("theimg");
       count = (count < MAX) ? count+1 : 1;
       imgEl.src = "images/"+(count)+".png";      
