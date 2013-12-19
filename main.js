f9cb4467a1da2af5780e982b9ed97621a2a746be
Maksim Lin <maks@manichord.com>
2013-12-19 12:12:46 +1100

    fix counter and make delay a var

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ window.onload = function() {
   console.log("init");
   
   var count = 1;
+  var DELAY = 25;//ms
+  var MAX = 100;
   
   setInterval(function() {
       var imgEl = document.getElementById("theimg");
-      imgEl.src = "images/"+(count++)+".png";
+      count = (count < MAX) ? count+1 : 1;
+      imgEl.src = "images/"+(count)+".png";      
       console.log("current src:"+imgEl.src);
-  }, 1 * 1000);
+  }, DELAY);
 }
